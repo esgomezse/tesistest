bea7210ba69e96b8516fc9b88d3a86b18a773828
jufarangoma <jufarangoma@gmail.com>
2020-05-22 21:08:01 -0500

    Amo a Guaca

---
+++ app/src/main/java/com/example/logintest/ui/login/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.example.logintest.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import com.example.logintest.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+
+    }
+}
